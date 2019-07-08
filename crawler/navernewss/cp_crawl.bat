7721d39c31e7b7ae5870c23af0ebb94c62461ba6
heme90 <46832888+heme90@users.noreply.github.com>
2019-07-08 18:31:38 +0900

    Update cp_crawl.bat

--- crawler/navernewss/cp_crawl.bat
+++ crawler/navernewss/cp_crawl.bat
@@ -5,10 +5,7 @@ start mongod  --dbpath c:\data\compathdata --logpath c:\data\compathdata\compath
 :1
 call C:\MyPython\news_engine\crawler\navernewss\newsone.py
 call C:\MyPython\news_engine\crawler\navernewss\analyzetesttest.py
-<<<<<<< HEAD
 call C:\MyPython\news_engine\crawler\navernewss\pymatrix_mk.py
 timeout -t 600 /nobreak
 goto 1
-=======
->>>>>>> parent of f693530... 20190626
 pause
